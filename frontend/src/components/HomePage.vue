fbd4329ed66241ed17085b8c955ff0876c9c5eb1
eloibusquetsgarcia-gonzalez <ebusquga7@alumnes.ub.edu>
2024-10-24 21:11:36 +0200

    feat: Created main page and connected windows.

---
+++ frontend/src/components/HomePage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <router-link to="/login" v-slot="{ href }">
+        <button :href="href" class="btn">Log In</button>
+      </router-link>
+
+      <router-link to="/account" v-slot="{ href }">
+        <button :href="href" class="btn">Sign Up</button>
+      </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage'
+}
+</script>
+
+<style>
+</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import HomePage from '@/components/HomePage'
+import SignUp from '@/components/SignUp'
+import LogIn from '@/components/LogIn'
 
 Vue.use(Router)
 
@@ -8,8 +10,19 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'HomePage',
+      component: HomePage
+    },
+    {
+      path: '/account',
+      name: 'SignUp',
+      component: SignUp
+    },
+    {
+      path: '/login',
+      name: 'LogIn',
+      component: LogIn
     }
+
   ]
 })
