73362cfc76fd3b10cbc377dc6e8039a927124b3d
Albert-23M <albertmeltec3@gmail.com>
2024-10-27 14:26:13 +0100

    Get, get_by_email and Post implemented with the new DB on cloud

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -4,15 +4,15 @@ from .base import SQLModel
 
 # Shared properties
 class UserBase(SQLModel):
+    name: str = Field(unique=False, index=True)
+    surname: str | None = None
+    username: str = Field(unique=True, index=True)
     email: str = Field(unique=True, index=True)
-    is_active: bool = True
-    is_superuser: bool = False
-    full_name: str | None = None
 
 
 # Database model, database table inferred from class name
 class User(UserBase, table=True):
-    id: int | None = Field(default=None, primary_key=True)
+    id_user: int | None = Field(default=None, primary_key=True)
     hashed_password: str
 
 # Properties to receive via API on creation
@@ -42,9 +42,9 @@ class UpdatePassword(SQLModel):
     current_password: str
     new_password: str
 
-# Properties to return via API, id is always required
+# Properties to return via API, id_user is always required
 class UserOut(UserBase):
-    id: int
+    id_user: int
 
 class UsersOut(SQLModel):
     data: list[UserOut]
