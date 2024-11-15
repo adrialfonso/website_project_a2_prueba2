c931a16bc518a4adb9bc2a1aa63e335db8bca6b8
ghs34 <gsydorenko03@gmail.com>
2024-11-15 17:34:34 +0100

    models/book.py implemented

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,9 +1,10 @@
 """ Main API routes definition """
 from fastapi import APIRouter
 
-from app.api.routes import login, users, utils
+from app.api.routes import login, users, utils #, books
 
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
+#api_router.include_router(books.router, prefix="/books", tags=["books"])
 #api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
---
+++ backend/app/models/book.py
@@ -0,0 +1,45 @@
+""" Book models """
+from sqlmodel import Field, SQLModel
+from typing import Optional
+
+# Shared properties
+class BookBase(SQLModel):
+    title: str = Field(index=True)
+    authors: str
+    synopsis: str | None = None
+    buy_link: str | None = None
+    genres: str
+    rating: float
+    editorial: str | None = None
+    comments: str | None = None
+    publication_date: str
+    image: str | None = None
+
+# Database model, table inferred from class name
+class Book(BookBase, table=True):
+    id_book: Optional[int] = Field(default=None, primary_key=True)
+
+# Properties to receive via API on creation
+class BookCreate(BookBase):
+    pass
+
+# Properties to receive via API on update, all are optional
+class BookUpdate(SQLModel):
+    title: Optional[str] = None
+    authors: Optional[str] = None
+    synopsis: Optional[str] = None
+    buy_link: Optional[str] = None
+    genres: Optional[str] = None
+    rating: Optional[float] = None
+    editorial: Optional[str] = None
+    comments: Optional[str] = None
+    publication_date: Optional[str] = None
+    image: Optional[str] = None
+
+# Properties to return via API
+class BookOut(BookBase):
+    id_book: int
+
+class BooksOut(SQLModel):
+    data: list[BookOut]
+    count: int
