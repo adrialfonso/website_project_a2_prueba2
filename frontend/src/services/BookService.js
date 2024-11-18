6921c758ec9b1e6d6735fc341776753438da98ed
Marta <mbernapo49@alumnes.ub.edu>
2024-11-18 19:44:51 +0100

    Book service

---
+++ frontend/src/services/BookService.js
@@ -0,0 +1,44 @@
+import http from '../http-common'
+
+class BookService {
+  readBooks (skip = 0, limit = 100) {
+    const config = {
+      headers: {
+        'accept': 'application/json'
+      },
+      params: {
+        skip: skip,
+        limit: limit
+      }
+    }
+
+    const path = '/api/v1/books'
+
+    return http.get(path, config)
+      .then((res) => {
+        return res
+      })
+      .catch((error) => {
+        return Promise.reject(error)
+      })
+  }
+  readTop5MatchedBooks (keyword) {
+    const config = {
+      headers: {
+        'accept': 'application/json'
+      }
+    }
+
+    const path = '/api/v1/books/' + keyword
+
+    return http.get(path, config)
+      .then((res) => {
+        return res
+      })
+      .catch((error) => {
+        return Promise.reject(error)
+      })
+  }
+}
+
+export default new BookService()
