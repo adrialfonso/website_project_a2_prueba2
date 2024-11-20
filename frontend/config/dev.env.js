960ab7bbf5e2b21cf04ab99a55b00aac8c7dccba
Marta <mbernapo49@alumnes.ub.edu>
2024-11-20 10:37:58 +0100

    Update configuration

--- frontend/config/dev.env.js
+++ frontend/config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_URL: '"https://website-project-a2-backend-4gg8.onrender.com"'
+  API_URL: '"http://localhost:8000"'
 })
